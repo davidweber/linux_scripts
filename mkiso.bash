92450f128bc642788481c6f3dc368ad4ae0a1efd
David Weber <dweber@oxefit.com>
2024-07-31 10:05:11 -0500

    update file header

--- mkiso.bash
+++ mkiso.bash
@@ -1,4 +1,26 @@
 #!/bin/bash
+#
+# @file   mkiso.bash
+# @author David Weber david.weber.dfw@gmail.com
+# @date   07/31/2024
+#
+# @brief mkiso module implementation
+#
+# Copyright (C) 2024 David Weber
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 if [ "$#" -lt "2" ]; then
   echo "usage: $0 <indir> <outfile>"
   exit;
